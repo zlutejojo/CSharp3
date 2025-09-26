8fdb3633aad9ce76cc4cb9e6bc93a718662d0dba
Jana Sebelova <Jana.Sebelova+swi@solarwinds.com>
2025-09-26 11:48:12 +0200

    v01

---
+++ sandbox/katas/Greed01/Greed/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, you!");
+Console.WriteLine("Házím kostkou a padla mi tato čísla:");
+Greed.Greed greed = new Greed.Greed();
+int[] dice = greed.rollDice();
+foreach (var die in dice)
+{
+    Console.Write(die + ", ");
+}
+Console.WriteLine();
+Console.WriteLine("Moje skóre je: " + greed.score(dice));
+
