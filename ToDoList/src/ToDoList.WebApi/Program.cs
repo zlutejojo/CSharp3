659dede92a9a8c81f6bca1e21ca093d81401dcad
jana-sebelova_swi <jana.sebelova@solarwinds.com>
2025-10-07 20:48:12 +0200

    03_Lesson_addControllerAndToDoItem

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,9 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+}
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/haf", () => "Haf, haf, haf!");
-app.MapGet("/mnau", () => "Mňau, mňau, mňau!");
-app.MapGet("/secti/{a:int}/{b:int}", (int a, int b) => $"Výsledek je {a} + {b} = {a + b}");
+{
+    app.MapControllers();
+}
 
 app.Run();
---
+++ ToDoList/src/ToDoList.WebApi/ToDoItemsController.cs
@@ -0,0 +1,51 @@
+namespace ToDoList.WebApi;
+
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.Models;
+using ToDoList.Domain.DTOs;
+
+[Route("api/[controller]")] //localhost:5000/api/ToDoItems
+[ApiController]
+public class ToDoItemsController : ControllerBase
+{
+    private static List<ToDoItem> items = [];
+
+    [HttpPost]
+    public IActionResult Create(ToDoItemCreateRequestDto request) //pouzijeme DTO - Data Transfer Object
+    {
+        return Ok(); //200
+    }
+
+    [HttpGet]
+    public IActionResult Read() //api/ToDoItems GET
+    {
+        return Ok(); //200
+    }
+
+    [HttpGet("{toDoItemId:int}")]
+    public IActionResult ReadById(int toDoItemId) //api/ToDoItems/<id> GET
+    {
+        try
+        {
+            throw new Exception("Neco se opravdu nepovedlo.");
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError); //500
+        }
+        return Ok(); //200
+    }
+
+    [HttpPut("{toDoItemId:int}")]
+    public IActionResult UpdateById(int toDoItemId, [FromBody]
+    ToDoItemUpdateRequestDto request)
+    {
+        return Ok(); //200
+    }
+
+    [HttpDelete("{toDoItemId:int}")]
+    public IActionResult DeleteById()
+    {
+        return Ok(); //200
+    }
+}
