1215fe38d4e1875f176f333249e717eef5100132
Jana <janacizikova@gmail.com>
2025-10-22 14:49:21 +0200

    WIP non working

--- ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
+++ ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
@@ -1,7 +1,10 @@
 namespace ToDoList.Domain.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ToDoItem
     {
+        [Key]
         public int ToDoItemId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
---
+++ ToDoList/src/ToDoList.Persistence/ToDoItemsContext.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDoList.Persistence;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Models;
+
+public class ToDoItemsContext : DbContext
+{
+    private readonly string connectionString;
+    public ToDoItemsContext(string connectionString = "DataSource=../../../../data/localdb.db")
+    {
+        this.connectionString = connectionString;
+    }
+
+    public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(connectionString);
+    }
+
+}
