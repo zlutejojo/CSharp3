bbb8ead3d08a58c4dce4bf3f369991619791d6ae
Jana <janacizikova@gmail.com>
2025-10-27 09:35:19 +0100

    add comments

--- ToDoList/src/ToDoList.Persistence/ToDoItemsContext.cs
+++ ToDoList/src/ToDoList.Persistence/ToDoItemsContext.cs
@@ -2,17 +2,21 @@
 
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Domain.Models;
-
+// řeší interakci s databází pomocí Entity Framework Core
 public class ToDoItemsContext : DbContext
 {
     private readonly string connectionString;
+    //pokud zavolám kontruktor bez parametrů, použije se výchozí connection string, pokud v konstrukturu použiji jiný connection string, použije se ten
     public ToDoItemsContext(string connectionString = "DataSource=../../data/localdb.db")
     {
         this.connectionString = connectionString;
+        // Metoda, která aplikuje všechny neaplikované migrace (pokud db neexistuje, vytvoří ji a aplikuje migrace, pokud db existuje, aplikuje chybějící migrace)
         this.Database.Migrate();
     }
+    // reprezentuje tabulku v databázi, která ukládá objekty ToDoItem
     public DbSet<ToDoItem> ToDoItems { get; set; }
-    //toto muzu pouzivat, protoze mam pridany balicek SQLite.EntityFrameworkCore
+    // nastavuje, že chci používat SQLite databázi a na které konkrétní databázi
+    // tuto metodu UseSqlite můžu používat, protože mám přidaný balíček SQLite.EntityFrameworkCore
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(connectionString);
