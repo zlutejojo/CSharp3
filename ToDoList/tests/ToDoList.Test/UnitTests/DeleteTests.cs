e9fba99aee86b807c23a221223f91eda19f9c09c
Jana <janacizikova@gmail.com>
2025-11-13 14:56:31 +0100

    add DeleteTests

--- ToDoList/tests/ToDoList.Test/UnitTests/DeleteTests.cs
+++ ToDoList/tests/ToDoList.Test/UnitTests/DeleteTests.cs
@@ -21,7 +21,7 @@ public class DeleteTests
     }
 
     [Fact]
-    public void Delete_DeletedItem_ReturnsNoContent()
+    public void Delete_DeleteByIdValidItemId_ReturnsNoContent()
     {
         // Arrange
         int existingId = 1;
@@ -38,17 +38,41 @@ public class DeleteTests
     }
 
     [Fact]
-    public void Delete_NonExistentItem_ReturnsNotFound()
+    public void Delete_DeleteByIdInvalidItemId_ReturnsNotFound()
     {
         // Arrange
         // předpokládám, že v seznamu není žádná položka s tímto ID
         int nonExistentId = 99999;
+        repositoryMock.GetById(nonExistentId).Returns((ToDoItem)null);
 
         // Act
         var actionResult = controller.DeleteById(nonExistentId);
 
         // Assert
+        repositoryMock.Received(1).GetById(nonExistentId);
         var notFoundResult = Assert.IsType<NotFoundResult>(actionResult);
         Assert.Equal(404, notFoundResult.StatusCode);
+    }
+
+    [Fact]
+    public void Delete_DeleteByIdUnhandledException_ReturnsInternalServerError()
+    {
+        // Arrange
+        var exceptionMessage = "Database connection failed";
+        int existingId = 1;
+        var itemToUpdate = new ToDoItem { ToDoItemId = existingId, Name = "Test Item", Description = "Test Description", IsCompleted = false };
+        repositoryMock.GetById(existingId).Returns(itemToUpdate);
+        repositoryMock.When(x => x.Delete(existingId))
+                      .Do(call => { throw new Exception(exceptionMessage); });
+
+        // Act
+        var actionResult = controller.DeleteById(existingId);
+
+        // Assert
+        var objectResult = Assert.IsType<ObjectResult>(actionResult);
+        Assert.Equal(500, objectResult.StatusCode);
+
+        var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+        Assert.Contains(exceptionMessage, problemDetails.Detail);
     }
 }
