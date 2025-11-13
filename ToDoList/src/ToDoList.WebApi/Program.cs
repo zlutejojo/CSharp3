8369c11d891af07a1b92b5f3db3511b1f9c35ca5
Jana <janacizikova@gmail.com>
2025-11-13 09:37:20 +0100

    add Swagger

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 {
     //Configure DI
     builder.Services.AddControllers();
+    builder.Services.AddSwaggerGen();
     builder.Services.AddDbContext<ToDoItemsContext>();
     builder.Services.AddScoped<IRepository<ToDoItem>, ToDoItemsRepository>();
 }
@@ -14,6 +15,8 @@ var app = builder.Build();
 {
     //Configure Middleware (HTTP request pipeline)
     app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI(config => config.SwaggerEndpoint("v1/swagger.json", "ToDoList API V1"));
 }
 
 app.Run();
