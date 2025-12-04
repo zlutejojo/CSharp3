8bffccfa3a456eb99bf00450ea53c1e13e664291
Jana <janacizikova@gmail.com>
2025-12-04 11:56:48 +0100

    add button for Delete item and Update IsCompleted

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -8,4 +8,5 @@ public interface IToDoItemsClient
     public Task<List<ToDoItemView>> ReadItemsAsync();
     public Task<ToDoItemView?> ReadItemByIdAsync(int itemId);
     public Task UpdateItemAsync(ToDoItemView item);
+    public Task DeleteItemAsync(int itemId);
 }
--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -52,4 +52,9 @@ public class ToDoItemsClient : IToDoItemsClient
         var itemRequest = new ToDoItemUpdateRequestDto(item.Name, item.Description, item.IsCompleted, item.Category);
         var response = await httpClient.PutAsJsonAsync($"api/ToDoItems/{item.Id}", itemRequest);
     }
+
+    public Task DeleteItemAsync(int itemId)
+    {
+        return httpClient.DeleteAsync($"api/ToDoItems/{itemId}");
+    }
 }
