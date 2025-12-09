c8a83e2c6ce17bf0f0c9db73f5ce3a91517091d9
Jana <janacizikova@gmail.com>
2025-12-09 19:05:16 +0100

    add Configuration, add frontend Validation

--- ToDoList/src/ToDoList.Frontend/Components/Models/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Components/Models/ToDoItemView.cs
@@ -1,12 +1,16 @@
 namespace ToDoList.Frontend.Models
 {
+using System.ComponentModel.DataAnnotations;
+
     public class ToDoItemView
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is mandatory.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Description is mandatory.")]
+        [StringLength(250, ErrorMessage = "Description cannot be longer than 250 characters.")]
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
         public string? Category { get; set; }
     }
 }
-
